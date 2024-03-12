b366352bc0ce5f0fb2eeb0f37a62e0d9ced23765
Yair Levi <yairlevi2001@gmail.com>
2024-03-12 21:25:26 +0200

    Add handler

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World, once again 4!')
+})
+
+app.post('/', (req, res) => {
+    const { body } = req
+    console.log(body)
+    res.status(200).json({
+        success: true,
+        content: body
+    })
 })
 
 app.listen(8000)
