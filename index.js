6e851079592b2fc286981020f607191fb1504aff
Yair Levi <yairlevi2001@gmail.com>
2024-03-12 20:59:16 +0200

    Testing change in API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World, once again!')
+    res.send('Hello World, once again 2!')
 })
 
 app.listen(8000)
