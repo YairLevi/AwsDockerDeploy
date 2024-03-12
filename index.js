ba19f94112cce1c3d4846ade649b74c898d29b72
Yair Levi <yairlevi2001@gmail.com>
2024-03-12 20:14:27 +0200

    Remove comments + new file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World, once again!')
 })
 
 app.listen(8000)
